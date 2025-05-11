1aad8a9a833e655d0b85e7a1458286c8d30e73d5
marcos_nb_na <marcosprunello@gmail.com>
2025-05-11 19:06:45 -0300

    Corrijo solución ejercicio 4, práctica 4

--- archivos/scripts_unidad4/evaluar_pelicula.R
+++ archivos/scripts_unidad4/evaluar_pelicula.R
@@ -9,7 +9,11 @@ if (n < 3 || n > 6) {
   cat("Cantidad inválida de jueces. Debe ser entre 3 y 6.\n")
 } else {
   cat("\nIngrese el nombre de la película:\n")
-  nombre <- scan(file = "stdin", what = character(), n = 1, quiet = TRUE)
+  nombre <- scan(file = "stdin", what = character(), n = 1, quiet = TRUE, sep = "*+-")
+  # Nota: scan separa el input donde ve espacios en blanco, para que no lo haga
+  # cambiamos el valor que tiene en su argumento sep, poniendo cualquier cosa
+  # que esperamos que nunca aparezca en un nombre de película, para que ningún
+  # nombre sea nunca separado.
   suma <- 0
   for (i in 1:n) {
     cat("\nIngrese la calificación del juez ", i, ":\n", sep = "")
